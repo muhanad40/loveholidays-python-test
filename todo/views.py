67d3933b5f9020f83421a1c42d8f3ea1ddbe0cff
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2018-09-25 12:26:12 +0100

    Reduce indentation

--- todo/views.py
+++ todo/views.py
@@ -4,18 +4,18 @@ from .models import Todo
 
 class TodoView(TemplateView):
 
-    template_name = "todo/index.html"
+  template_name = "todo/index.html"
 
-    def get_context_data(self, **kwargs):
-      context = super(TodoView, self).get_context_data(**kwargs)
-      context['tasks'] = Todo.objects.all()
-      return context
+  def get_context_data(self, **kwargs):
+    context = super(TodoView, self).get_context_data(**kwargs)
+    context['tasks'] = Todo.objects.all()
+    return context
 
-    def post(self, request, **kwargs):
-      task = request.POST
-      new_todo = Todo(description=task.get('description'), is_complete=False)
-      new_todo.save()
-      return redirect('home')
+  def post(self, request, **kwargs):
+    task = request.POST
+    new_todo = Todo(description=task.get('description'), is_complete=False)
+    new_todo.save()
+    return redirect('home')
 
 def RemoveTodo(request, id):
   task = Todo.objects.get(id=id)
